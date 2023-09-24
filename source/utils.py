e1ba6c26b33dd295d5c448e49c9cfbf5879b5809
Quibusque <cruck990@gmail.com>
2023-09-24 19:41:54 +0200

    added prepare_dataframe_and_files_for_training It moves images around in folder and generates the dataframes to be used in the further steps of training

--- source/utils.py
+++ source/utils.py
@@ -72,3 +72,58 @@ def sample_classes(
     df = df.sort_values(by=file_name_key).reset_index(drop=True)
 
     return df
+
+
+def prepare_dataframe_and_files_for_training(
+    df: pd.DataFrame,
+    chosen_labels: list,
+    file_name_key: str,
+    label_key: str,
+    img_dir: str,
+    bad_img_dir: str,
+    test_img_dir: str,
+    num_samples: int,
+    seed: int,
+):
+    """
+    Given a list of labels, this function:
+    1. Moves all images whose labels are not in the list to the bad_img_dir
+    2. Creates a dataframe with at most num_samples images for each label
+    3. Moves excess images that were not sampled to the test_img_dir
+
+    Args:
+        df (pd.DataFrame): dataframe with the labels
+        chosen_labels (list): list of labels to keep
+        file_name_key (str): name of the column containing the file names
+        label_key (str): name of the column containing the labels
+        img_dir (str): path to the image directory
+        bad_img_dir (str): path to the directory to move bad images to
+        test_img_dir (str): path to the directory to move test images to
+        num_samples (int): maximum number of samples for each label
+        seed (int): seed for random shuffling
+
+    Returns:
+        df_good (pd.DataFrame): dataframe with the sampled good labels
+        df_test (pd.DataFrame): dataframe with the test images
+    """
+    # create the dataframe with the images that have the good labels
+    df_good = df[df[label_key].isin(chosen_labels)]
+
+    # move all images that have bad labels to the bad_img_dir
+    df_bad = df[~df[label_key].isin(chosen_labels)]
+    for file_name in df_bad[file_name_key]:
+        os.rename(
+            os.path.join(img_dir, file_name), os.path.join(bad_img_dir, file_name)
+        )
+
+    # sample num_samples images from each class
+    df_good = sample_classes(df_good, label_key, num_samples, file_name_key, seed)
+
+    # move all images that are not in df_good to the test_img_dir
+    df_test = df[~df[file_name_key].isin(df_good[file_name_key])]
+    for file_name in df_test[file_name_key]:
+        os.rename(
+            os.path.join(img_dir, file_name), os.path.join(test_img_dir, file_name)
+        )
+
+    return df_good, df_test
