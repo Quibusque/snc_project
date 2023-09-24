36163d7faeb2ebe7cbb4f71948606a09dda2e1fb
Quibusque <cruck990@gmail.com>
2023-09-24 19:41:06 +0200

    added sample_classes function to utils.py It samples a number of entries from each label of a dataframe

--- source/utils.py
+++ source/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 
-def format_id_to_filename(df: pd.DataFrame, file_name_key:str) -> pd.DataFrame:
+def format_id_to_filename(df: pd.DataFrame, file_name_key: str) -> pd.DataFrame:
     """
     Changes the name of the "id" column of the dataframe to file_name_key and
     appends ".jpeg" to each entry in the column.
@@ -15,12 +15,12 @@ def format_id_to_filename(df: pd.DataFrame, file_name_key:str) -> pd.DataFrame:
         pd.DataFrame: modified dataframe
     """
     df["id"] = df["id"].apply(lambda x: x + ".jpeg")
-    
+
     df = df.rename(columns={"id": file_name_key})
     return df
 
 
-def remove_wrong_entries(df: pd.DataFrame, img_dir: str, file_name_key:str) -> None:
+def remove_wrong_entries(df: pd.DataFrame, img_dir: str, file_name_key: str) -> None:
     """
     Removes entries from the dataframe that do not have corresponding files in
     the image directory.
@@ -37,4 +37,38 @@ def remove_wrong_entries(df: pd.DataFrame, img_dir: str, file_name_key:str) -> N
 
     # remove entries with df[file_name_key] in missing_files
     df = df[~df[file_name_key].isin(missing_files)]
+    return df
+
+
+def sample_classes(
+    df: pd.DataFrame, label_key: str, num_samples: int, file_name_key: str, seed: int
+) -> pd.DataFrame:
+    """
+    This function takes a dataframe with a column of labels and returns
+    a dataframe with at most num_samples entries for each label.
+
+    Before sampling, the dataframe is randomly shuffled with seed.
+    The dataframe is sorted by alphabetical order of file_name_key after sampling
+    because this is the order that the images are loaded in by the tf dataset.
+
+    Args:
+        df (pd.DataFrame): dataframe to sample from
+        label_key (str): name of the column containing the labels
+        num_samples (int): maximum number of samples for each label
+        file_name_key (str): name of the column containing the file names
+        seed (int): seed for random shuffling
+
+    Returns:
+        pd.DataFrame: sampled dataframe
+    """
+    # shuffle the dataframe with seed
+    df = df.sample(frac=1, random_state=seed).reset_index(drop=True)
+
+    # Create a mask to filter rows so that each class has at most num_samples entries
+    mask = df.groupby(label_key).cumcount() < num_samples
+    df = df[mask]
+
+    # sort dataframe by alphabetical order of file_name column
+    df = df.sort_values(by=file_name_key).reset_index(drop=True)
+
     return df
