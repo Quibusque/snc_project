4dbf1a2e464314da02c330984dc52f785fed780d
Francesco Zenesini <fra.zene.z@gmail.com>
2023-09-25 14:54:07 +0200

    added docstrings to training.py

--- source/training.py
+++ source/training.py
@@ -12,13 +12,39 @@ def build_dataset(
     img_dir: str,
     df_good: pd.DataFrame,
     label_key: str,
+    shuffle: bool,
+    seed: int,
     validation_split: float = None,
     image_size: tuple = (256, 256),
     crop_to_aspect_ratio: bool = True,
     batch_size: int = 64,
-    shuffle: bool = False,
-    seed: int = None,
-):
+) -> tf.data.Dataset:
+    """
+    This function builds a tf dataset from a directory of images and a dataframe
+    with the image file names and labels.
+    If validation_split is not None, the dataset is split into training and
+    validation sets.
+
+    Args:
+        img_dir (str): path to image directory
+        df_good (pd.DataFrame): dataframe with image file names and labels
+        label_key (str): name of the column containing the labels
+        shuffle (bool): whether to shuffle the dataset
+        seed (int): seed for shuffling
+        validation_split (float): fraction of the dataset to use for
+            validation. Defaults to None.
+        image_size (tuple): size of the images in the dataset. Defaults to (256,256).
+        crop_to_aspect_ratio (bool): whether to crop the images to the aspect
+            ratio of image_size. Defaults to True.
+        batch_size (int): batch size. Defaults to 64.
+
+    Returns:
+        if validation_split is None:
+            dataset (tf.data.Dataset): dataset of images and labels
+        if validation_split is not None:
+            training (tf.data.Dataset): training dataset
+            validation (tf.data.Dataset): validation dataset
+    """
     # set subset variable based on validation_split
     if validation_split is None:
         subset = None
@@ -51,13 +77,29 @@ def build_dataset(
 
 
 def build_model(
-    num_classes,
-    metric,
-    loss,
-    dropout_rate,
-    input_shape=(256, 256, 3),
-    print_summary=False,
-):
+    num_classes: int,
+    metric: tf.keras.metrics.Metric,
+    loss: tf.keras.losses.Loss,
+    dropout_rate: float = 0.2,
+    input_shape: tuple = (256, 256, 3),
+    print_summary: bool = True
+)-> tf.keras.models.Model:
+    """
+    This function builds a model with the EfficientNetV2B0 network as the base
+    and a dense layer with softmax activation as the head. The model is compiled
+    with the Adam optimizer, the specified loss function and the specified metric.
+
+    Args:
+        num_classes (int): number of classes
+        metric (tf.keras.metrics.Metric): metric to use for evaluation
+        loss (tf.keras.losses.Loss): loss function
+        dropout_rate (float): dropout rate. Defaults to 0.2.
+        input_shape (tuple): shape of the input images. Defaults to (256,256,3).
+        print_summary (bool): whether to print the model summary. Defaults to True.
+
+    Returns:
+        model (tf.keras.models.Model): compiled model
+    """
     model = tf.keras.models.Sequential(
         [
             tf.keras.applications.efficientnet_v2.EfficientNetV2B0(
@@ -94,7 +136,30 @@ def train_model(
     patience=3,
     save_weights_only=True,
     save_best_only=True,
-):
+)->tf.keras.callbacks.History:
+    """
+    This function trains a model on the training data and evaluates it on the
+    validation data. Early stopping is used to stop training if the validation
+    loss does not improve for patience epochs. The model weights are saved to
+    "checkpoint_dir/model_name" after each epoch. 
+
+    Args:
+        model (tf.keras.models.Model): model to train
+        training_data (tf.data.Dataset): training dataset
+        validation_data (tf.data.Dataset): validation dataset
+        epochs (int): number of epochs
+        model_name (str): name of the model (used for saving the weights)
+        checkpoint_dir (str): path to the directory to save the weights to
+        patience (int): number of epochs to wait before stopping training if
+            the validation loss does not improve. Defaults to 3.
+        save_weights_only (bool): whether to save only the weights or the whole
+            model. Defaults to True.
+        save_best_only (bool): whether to save only the weights of the best
+            model. Defaults to True.
+    Returns:
+        history (tf.keras.callbacks.History): history object containing training
+            and validation metrics
+    """
     history = model.fit(
         training_data,
         validation_data=validation_data,
